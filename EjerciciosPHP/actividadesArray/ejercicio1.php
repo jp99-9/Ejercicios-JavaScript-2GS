8b99b75786169f21b14bafad00e254ac52d342e6
Jp 2GS <jaristizabaluchima@iessonferrer.net>
2024-10-03 18:24:06 +0200

    Ejercicio2 php

--- EjerciciosPHP/actividadesArray/ejercicio1.php
+++ EjerciciosPHP/actividadesArray/ejercicio1.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     //Crear un array
 
     $color = array('white', 'green', 'red', 'blue', 'black');
+    // Display a sentence using elements from the color array
+    echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon" . "\n";
     ?>
 </body>
+
 </html>
